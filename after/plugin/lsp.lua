f535d266fa01ed63550ab143608ed7ed1d599cc1
Julio Gonzalez <julio.gonzalez@datadoghq.com>
2024-12-19 09:49:39 +0100

    tsserver no longer works.
    Replace tsserver in favor of ts_ls as the required lsp plugins.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'clangd', 'eslint'},
+  ensure_installed = {'ts_ls', 'rust_analyzer', 'clangd', 'eslint'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
